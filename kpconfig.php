8c543887e1516e56e998241c645916906f22370a
David Younker <dysproseum@gmail.com>
2024-04-11 11:26:17 -0400

    Allow kpconfig, add kpconfig docker settings

---
+++ kpconfig.php
@@ -0,0 +1,14 @@
+<?php
+$cfg['archivemode'] = true;
+$cfg['dateformat'] = 'm.d.y H:i';
+$cfg['timeformat'] = 'H:i';
+$cfg['smalldateformat'] = 'm.d.y';
+$cfg['laststreamscount'] = 12;
+$cfg['window_x'] = 420;
+$cfg['window_y'] = 420;
+$cfg['enablegetid3'] = 1;
+$cfg['db_host'] = 'database';
+$cfg['db_name'] = 'docker';
+$cfg['db_user'] = 'docker';
+$cfg['db_pass'] = 'docker';
+$cfg['db_prepend'] = '';
