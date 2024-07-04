d2b0f5a08dd75069a724b2b0990557e14a8135fd
David Younker <dysproseum@gmail.com>
2024-07-04 00:55:24 -0400

    Fix seek while streaming, allow seek on mobile

--- index.php
+++ index.php
@@ -367,7 +367,6 @@ $cfg['jw6_enable'] = false;
 // end of configuration
 if (file_exists('kpconfig.php')) include('kpconfig.php');
 
-
 function geturi()
 {
 	global $phpenv;
@@ -13606,6 +13605,93 @@ function playresource2($sid)
 	}
 }
 
+function seek_stream($sid)
+{
+	global $cfg;
+
+	$f2 = new file2($sid, true);
+	$fdesc = new filedesc($f2->fname);
+	$filesize = $f2->fsize;
+	$length = $filesize;
+
+	// Set custom streaming buffer size.
+	if (isset($cfg['stream_buffer_size']))
+	{
+		$buffer_size = $cfg['stream_buffer_size'];
+	} else
+	{
+		$buffer_size = 1024 * 1024;
+	}
+
+	if (isset($_SERVER['HTTP_RANGE'])) {
+		preg_match('/bytes=(\d+)-(\d+)?/', $_SERVER['HTTP_RANGE'], $matches);
+		$offset = intval($matches[1]);
+		if (!isset($matches[2])) {
+		        $end = $offset + $buffer_size;
+		        if ($end > $filesize - 1) {
+		                $end = $filesize - 1;
+		        }
+		}
+		else {
+		        $end = $matches[2];
+		}
+		$length = $end + 1 - $offset;
+		header("HTTP/1.1 206 Partial content");
+		header("Content-Range: bytes $offset-$end/$filesize");
+	}
+
+	// Override mime type so flac will stream.
+	if (isset($cfg['stream_flac_mime_override']))
+	{
+		header('Content-Type: ' . $cfg['stream_flac_mime_override']);
+	} else
+	{
+		header('Content-Type: ' . $fdesc->mime);
+	}
+
+	header('Content-Disposition: attachment; filename="' . $f2->fname.'"');
+	header('Connection: Keep-Alive');
+	header('Content-Transfer-Encoding: binary');
+	header('Accept-Ranges: bytes');
+	header('Content-Length: ' . $length);
+
+	$fp = fopen($f2->fullpath, 'rb');
+	fseek($fp, $offset);
+	while (!feof($fp))
+	{
+	        $buffer = fread($fp, 32 * 1024);
+	        print $buffer;
+	}
+	fclose($fp);
+	exit;
+}
+
+function seek_stream_mobile($sid)
+{
+	global $cfg;
+
+	$f2 = new file2($sid, true);
+	$fdesc = new filedesc($f2->fname);
+	$filename = $f2->fullpath;
+	$length = filesize($filename);
+
+	// Override mime type so flac will stream.
+	if (isset($cfg['stream_flac_mime_override']))
+	{
+		header('Content-Type: ' . $cfg['stream_flac_mime_override']);
+	} else
+	{
+		header('Content-Type: ' . $fdesc->mime);
+	}
+
+	// Allow seek on mobile - prevent "Live Broadcast" in player.
+	$fp = fopen($filename, "rb");
+	header("Content-Length: $length");
+	header('Content-Disposition: attachment; filename="' . $f2->fname.'"');
+	fpassthru($fp);
+	exit;
+}
+
 class kq_Measure
 {
 	function getmicrotime()
@@ -14535,6 +14621,12 @@ function parseurl($url, $title = '', $artist = '', $album = '')
 	return $urlr;
 }
 
+function is_mobile() {
+	$useragent=$_SERVER['HTTP_USER_AGENT'];
+	if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4))) return true;
+	else return false;
+}
+
 function print_html5container() {
   if (!HTML5VIDEO && !HTML5AUDIO) {
     return;
@@ -14610,7 +14702,12 @@ function print_file($sid, $showlink=0, $includeabsolute=0, $f2=false, $smarksid 
 
 	if (HTML5AUDIO && $pathinfo['extension'] !== 'jpg') {
 		echo '<span class="file html5audio">';
-		echo '<a title="Play HTML5 audio" href="'. PHPSELF. "?downloadfile=".$sid.'&amp;c='.$u_cookieid.$urlextra.'" onclick="return play_html5audio(this);" class="html5audio">';
+		if (is_mobile()) {
+			echo '<a title="Play HTML5 audio on mobile" href="'. PHPSELF. "?seek_stream_mobile=".$sid.'&amp;c='.$u_cookieid.$urlextra.'" onclick="return play_html5audio(this);" class="html5audio">';
+		}
+		else {
+			echo '<a title="Play HTML5 audio" href="'. PHPSELF. "?seek_stream=".$sid.'&amp;c='.$u_cookieid.$urlextra.'&file=.mp3" onclick="return play_html5audio(this);" class="html5audio">';
+		}
 		echo '<img src="images/html5audio.gif" alt="Play HTML5 audio" border="0" />';
 		echo '</a></span> ';
 	}
@@ -16731,6 +16828,8 @@ foreach($_GET as $name => $val)
 		case 'imgsid':
 		case 'imgid3sid':
 		case 'templist':
+		case 'seek_stream':
+		case 'seek_stream_mobile':
 			$a = $name;
 		break;
 	}
@@ -16795,6 +16894,14 @@ if (strlen($a) > 0)
 				if (chksecurity(frm_get('sid', 1))) playresource2(frm_get('sid', 1));
 			break;
 			
+			case 'seek_stream':
+				seek_stream(frm_get('seek_stream', 1));
+			break;
+
+			case 'seek_stream_mobile':
+				seek_stream_mobile(frm_get('seek_stream_mobile', 1));
+			break;
+
 			case 'streamplaylist':
 				if (frm_isset('extm3u')) $valuser->set('extm3u', 1);
 				if (frm_ok('streamplaylist', 1))
