79950bfef012989aac96e527838f8783d4f32948
David Younker <dysproseum@gmail.com>
2024-07-17 19:45:05 -0400

    Store trackListing in player iframe

--- kptheme/html5_player/player.php
+++ kptheme/html5_player/player.php
@@ -5,11 +5,33 @@
 <link href="html5_player.css" rel="stylesheet" type="text/css" />
 <link href='mobile.css' rel='stylesheet' media='only screen and (max-width: 768px)' type='text/css' />
 <script type="text/javascript">
+  let player;
+  let childCallbacks = [];
+  // Store tracks in player iframe in case the index page changes.
+  let trackListing = [];
+  let current;
+
   // Player callback.
-  function playerFrame(track) {
-    player.hidden = false;
-    player.src=track;
-    player.play();
+  // tracks: trackListing array.
+  // num: optional pointer to track number.
+  function playerFrame(tracks, num=0) {
+    trackListing = tracks;
+    current = tracks[num];
+    player.pause();
+    player.src=tracks[num];
+    var playPromise = player.play();
+
+    // In browsers that don’t yet support this functionality,
+    // playPromise won’t be defined.
+    if (playPromise !== undefined) {
+      playPromise.then(function() {
+        // Automatic playback started!
+      }).catch(function(error) {
+        // Automatic playback failed.
+        // Show a UI element to let the user manually start playback.
+        console.log(error);
+      });
+    }
   }
 
   // Register callback.
@@ -19,6 +41,38 @@
       parent.registerPlayerChild(playerFrame);
     }
   };
+
+  window.addEventListener("load", function() {
+    childCallbacks = parent.getIndexCallbacks();
+    player = document.getElementById("html5player");
+
+    player.addEventListener("ended", function() {
+      console.log("ended");
+      var nextSong;
+
+      if (trackListing.length > 0) {
+        for (i=0; i<trackListing.length; i++) {
+          if (trackListing[i] == current) {
+            if (trackListing[i + 1]) {
+              nextSong = trackListing[i + 1];
+            }
+            else {
+              return false;
+            }
+          }
+        }
+      }
+
+      current = nextSong;
+      player.pause();
+      player.src = nextSong;
+      player.play();
+    });
+
+    player.addEventListener("playing", function() {
+      console.log("playing");
+    });
+  });
 </script>
 </head>
 <body>
@@ -32,32 +86,5 @@ echo '<div id="html5container">';
 echo '<' . $player_type . ' id="html5player" controls></video>';
 echo '</div>';
 ?>
-
-<script type="text/javascript">
-  let player;
-  let childCallbacks = [];
-
-  window.addEventListener("load", function() {
-    childCallbacks = parent.getIndexCallbacks();
-    player = document.getElementById("html5player");
-
-    player.addEventListener("ended", function() {
-      console.log("ended");
-      var nextSong;
-      for (var i=0; i < childCallbacks.length; i++){
-        var callback = childCallbacks[i];
-        var currentSong = callback();
-        var nextSong = callback(currentSong);
-      }
-      player.pause();
-      player.src = nextSong;
-      player.play();
-    });
-
-    player.addEventListener("playing", function() {
-      console.log("playing");
-    });
-  });
-</script>
 </body>
 </html>
