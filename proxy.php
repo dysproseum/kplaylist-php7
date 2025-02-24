108deac9270cecfabb04a880ed4e781c870615cf
David Younker <dysproseum@gmail.com>
2025-02-24 12:58:56 -0500

    Update links to absolute urls in iframe proxy

--- proxy.php
+++ proxy.php
@@ -1,7 +1,26 @@
-<?php 
+<?php
 
 $url = $_GET['url'];
 
-$output= file_get_contents($url);
+$html = file_get_contents($url);
+if (!$html) {
+  // @todo check status codes https://stackoverflow.com/a/52662522
+  print "Failed to load";
+  exit;
+}
 
-print $output ? $output : "Failed to load";
+// $html=<<<HTML
+// <img src="/relative/url/img.jpg" />
+// <form action="/">
+// <a href='/relative/url/'>Note the Single Quote</a>
+// <img src="//site.com/protocol-relative-img.jpg" />
+// HTML;
+//
+// $base='https://example.com';
+//
+// echo preg_replace('~(?:src|action|href)=[\'"]\K/(?!/)[^\'"]*~',"$base$0",$html);
+
+$base = parse_url($url, PHP_URL_SCHEME) . "://" . parse_url($url, PHP_URL_HOST);
+
+echo preg_replace('~(?:src|action|href)=[\'"]\K/(?!/)[^\'"]*~',"$base$0",$html);
+
